6a85a2af6748e5291c583ddde118e4188e79882c
Seongil Lee <silee@cookapps.com>
2022-07-20 17:21:29 +0900

    Timezone 오류 수정

--- src/helpers/time.helper.ts
+++ src/helpers/time.helper.ts
@@ -1,11 +1,12 @@
+import { DEFAULT_TIMEZONE } from '../constants/time.constant';
 import * as dateFns from 'date-fns';
 import * as dateFnsTz from 'date-fns-tz';
 
 export const format = ({
   date,
   format = 'yyyy-MM-dd HH:mm:ss',
-  tz = 'Asia/Seoul',
+  tz = DEFAULT_TIMEZONE,
 }) => {
-  const utcTime = dateFnsTz.zonedTimeToUtc(date, tz);
-  return dateFns.format(utcTime, format);
+  const zonedTime = dateFnsTz.utcToZonedTime(new Date(date), tz);
+  return dateFns.format(zonedTime, format);
 };
